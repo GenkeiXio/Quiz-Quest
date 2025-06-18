a00586d7659ac571758985ba3e81c54fee9a1ccb
PrinceLouisJaylo <158171420+GenkeiXio@users.noreply.github.com>
2025-06-18 08:39:23 +0800

    QuizQuest

---
+++ QuizQuestions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who is the author of the classic science fiction novel Dune?",
+        answer: "D. Frank Herbert",
+        options: [
+            "A. Isaac Asimov",
+            "B. Philip K. Dick",
+            "C. Arthur C. Clarke",
+            "D. Frank Herbert"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "In which science fiction TV series does the starship USS Enterprise play a central role?",
+        answer: "A. Star Trek",
+        options: [
+            "A. Star Trek",
+            "B. Battlestar Galactica",
+            "C. The Expanse",
+            "D. Firefly"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which famous sci-fi film features a character named HAL 9000, an artificial intelligence computer?",
+        answer: "C. 2001: A Space Odyssey of 1968",
+        options: [
+            "A. Blade Runner",
+            "B. The Matrix",
+            "C. 2001: A Space Odyssey of 1968",
+            "D. War of the Worlds"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who wrote the dystopian novel 1984, a classic of science fiction literature?",
+        answer: "B. George Orwell",
+        options: [
+            "A. Aldous Huxley",
+            "B. George Orwell",
+            "C. Ray Bradbury",
+            "D. H.G. Wells"
+        ]
+    },
+    {
+        numb: 5,
+        question: "In the movie The Matrix, what is the real name of the protagonist who goes by the alias Neo?",
+        answer: "B. Thomas A. Anderson",
+        options: [
+            "A. John Smith",
+            "B. Thomas A. Anderson",
+            "C. Kevin Johnson",
+            "D. William Adams"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Who is the God of the sky and is considered the ruler, protector and father of all gods and humans?",
+        answer: "C. Zeus",
+        options: [
+            "A. Hades",
+            "B. Poseidon",
+            "C. Zeus",
+            "D. Hermes"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which Greek hero is known for his incredible strength and the twelve labors he performed?",
+        answer: "D. Hercules",
+        options: [
+            "A. Perseus",
+            "B. Achilles",
+            "C. Theseus",
+            "D. Hercules"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Who is the goddess of wisdom, courage, and strategic warfare in Greek mythology?",
+        answer: "B. Athena",
+        options: [
+            "A. Aphrodite",
+            "B. Athena",
+            "C. Artemis",
+            "D. Demeter"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What creature, half-man and half-bull, resided in the labyrinth of King Minos in Greek mythology?",
+        answer: "A. Minotaur",
+        options: [
+            "A. Minotaur",
+            "B. Centaur",
+            "C. Harpy",
+            "D. Chimera"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which mythological hero is known for his journey to the Underworld to rescue his wife, Eurydice?",
+        answer: "A. Orpheus",
+        options: [
+            "A. Orpheus",
+            "B. Perseus",
+            "C. Odysseus",
+            "D. Jason"
+        ]
+    },
+];
